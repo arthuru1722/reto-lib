aa7792c01eec584bbe69faaa5cd2a15025cad1e5
Arthuru <112415136+arthuru1722@users.noreply.github.com>
2025-06-10 19:13:50 -0300

    Add files via upload

---
+++ livros.js
@@ -0,0 +1,156 @@
+const livros = [
+  {
+    id: 1,
+    titulo: "Peter Pan",
+    autor: "J.M. Barrie",
+    capa: "https://example.com/peter_pan_capa.jpg",
+    dataLancamento: "1911",
+    categorias: ["Literatura infantil"],
+    paginas: 224,
+    tempoLeitura: 6,
+    quizUrl: "peter_pan_quiz"
+  },
+  {
+    id: 2,
+    titulo: "Alice no País das Maravilhas",
+    autor: "Lewis Carroll",
+    capa: "https://example.com/alice_capa.jpg",
+    dataLancamento: "1865",
+    categorias: ["Literatura infantil"],
+    paginas: 201,
+    tempoLeitura: 5,
+    quizUrl: "alice_pais_maravilhas_quiz"
+  },
+  {
+    id: 3,
+    titulo: "O menino que entregava leite",
+    autor: "Regina Rennó",
+    capa: "https://example.com/menino_leite.jpg",
+    dataLancamento: "2014",
+    categorias: ["Literatura infantil"],
+    paginas: 32,
+    tempoLeitura: 1,
+    quizUrl: "menino_entregava_leite_quiz"
+  },
+  {
+    id: 4,
+    titulo: "A Laranja Colorida",
+    autor: "Ronaldo Simões Coelho",
+    capa: "https://example.com/laranja_colorida.jpg",
+    dataLancamento: "2011",
+    categorias: ["Literatura infantil"],
+    paginas: 36,
+    tempoLeitura: 1,
+    quizUrl: "laranja_colorida_quiz"
+  },
+  {
+    id: 5,
+    titulo: "Dormir fora de casa",
+    autor: "Ronaldo Simões Coelho",
+    capa: "https://example.com/dormir_fora.jpg",
+    dataLancamento: "2009",
+    categorias: ["Literatura infantil"],
+    paginas: 32,
+    tempoLeitura: 1,
+    quizUrl: "dormir_fora_casa_quiz"
+  },
+  {
+    id: 6,
+    titulo: "Cobras e Lagartos",
+    autor: "Wânia Amarante",
+    capa: "https://example.com/cobras_lagartos.jpg",
+    dataLancamento: "2008",
+    categorias: ["Literatura infantil"],
+    paginas: 40,
+    tempoLeitura: 1,
+    quizUrl: "cobras_lagartos_quiz"
+  },
+  {
+    id: 7,
+    titulo: "Helena",
+    autor: "Machado de Assis",
+    capa: "https://example.com/helena.jpg",
+    dataLancamento: "1876",
+    categorias: ["Literatura Brasileira-Romance"],
+    paginas: 224,
+    tempoLeitura: 5,
+    quizUrl: "helena_quiz"
+  },
+  {
+    id: 8,
+    titulo: "Dom Casmurro",
+    autor: "Machado de Assis",
+    capa: "https://m.media-amazon.com/images/I/71k7mUfJ4pL._AC_UF1000,1000_QL80_.jpg",
+    dataLancamento: "1899",
+    categorias: ["Literatura Brasileira-Romance"],
+    paginas: 256,
+    tempoLeitura: 6,
+    quizUrl: "dom_casmurro_quiz"
+  },
+  {
+    id: 9,
+    titulo: "Esaú e Jacó",
+    autor: "Machado de Assis",
+    capa: "https://example.com/esau_jaco.jpg",
+    dataLancamento: "1904",
+    categorias: ["Literatura Brasileira-Romance"],
+    paginas: 296,
+    tempoLeitura: 7,
+    quizUrl: "esau_jaco_quiz"
+  },
+  {
+    id: 10,
+    titulo: "Quincas Borba",
+    autor: "Machado de Assis",
+    capa: "https://example.com/quincas_borba.jpg",
+    dataLancamento: "1892",
+    categorias: ["Literatura Brasileira-Romance"],
+    paginas: 360,
+    tempoLeitura: 9,
+    quizUrl: "quincas_borba_quiz"
+  },
+  {
+    id: 11,
+    titulo: "O Mulato",
+    autor: "Aluísio Azevedo",
+    capa: "https://example.com/o_mulato.jpg",
+    dataLancamento: "1881",
+    categorias: ["Literatura Brasileira-Romance"],
+    paginas: 240,
+    tempoLeitura: 6,
+    quizUrl: "o_mulato_quiz"
+  },
+  {
+    id: 12,
+    titulo: "Memórias Póstumas de Brás Cubas",
+    autor: "Machado de Assis",
+    capa: "https://example.com/memorias_bras_cubas.jpg",
+    dataLancamento: "1881",
+    categorias: ["Literatura Brasileira-Romance"],
+    paginas: 240,
+    tempoLeitura: 7,
+    quizUrl: "memorias_postumas_bras_cubas_quiz"
+  },
+  {
+    id: 13,
+    titulo: "O Cortiço",
+    autor: "Aluísio Azevedo",
+    capa: "https://example.com/o_cortico.jpg",
+    dataLancamento: "1890",
+    categorias: ["Literatura Brasileira-Romance"],
+    paginas: 320,
+    tempoLeitura: 8,
+    quizUrl: "o_cortico_quiz"
+  },
+  {
+    id: 14,
+    titulo: "Senhora",
+    autor: "José de Alencar",
+    capa: "https://example.com/senhora.jpg",
+    dataLancamento: "1875",
+    categorias: ["Literatura Brasileira-Romance"],
+    paginas: 320,
+    tempoLeitura: 8,
+    quizUrl: "senhora_quiz"
+  }
+];

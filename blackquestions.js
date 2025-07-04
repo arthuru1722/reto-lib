0444ea542c625b99df328f0b1472b298995337d1
Arthuru <112415136+arthuru1722@users.noreply.github.com>
2025-07-03 23:13:21 -0300

    Create blackquestions.js

---
+++ blackquestions.js
@@ -0,0 +1,559 @@
+const quizzes = {
+    Quiz_1: {
+        perguntas: [
+            {
+                id: 1,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 2,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 3,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 4,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 5,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 6,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 7,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 8,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 9,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 10,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 11,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 12,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 13,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 14,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 15,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 16,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 17,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 18,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 19,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 20,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 21,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 22,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 23,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 24,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 25,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            }
+        ]
+    }, Quiz_2: {
+        perguntas: [
+            {
+                id: 1,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 2,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 3,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 4,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 5,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 6,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 7,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 8,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 9,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 10,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 11,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 12,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 13,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 14,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 15,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 16,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 17,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 18,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 19,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 20,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 21,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 22,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 23,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 24,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            },
+            {
+                id: 25,
+                pergunta: "Pergunta Placeholder",
+                opcoes: [
+                    "correta",
+                    "incorreta",
+                    "incorreta",
+                    "incorreta",
+                ],
+                respostaCorreta: 0
+            }
+        ]
+    }
+}
